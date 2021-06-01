caf80f3a274e921c7afa7e27f20908ef19ab49f7
Vitor <vitorhenrique018@gmail.com>
2021-06-01 15:33:58 -0300

    criacao rota auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const User = require("../models/User");
+const bcrypt = require("bcryptjs")
 
 const router = express.Router();
 
@@ -20,5 +21,22 @@ router.post("/register", async (req, res) => {
     return res.status(400).send({ error: "Registration failed" });
   }
 });
+
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await (await User.findOne({ email })).isSelected('+password');
+
+  if(!user) {
+    return res.status(400).send({ error: 'User not found '});
+  }
+
+  if(!await bcrypt.compare(password, user.password)){ //comparando as duas senhas nao batem
+    return res.status(400).send({ error: 'Invalid Password'});
+  }
+
+  res.send({ user });
+
+})
 
 module.exports = (app) => app.use("/auth", router); //repassando o router pro app com o prefixo auth
